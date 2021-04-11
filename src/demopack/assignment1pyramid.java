d767f817dab3d1edfe21ea59885c1c1af2357b12
anilkalshetti <anilkalshetti@gmail.com>
2021-04-11 22:59:40 +0530

    Notes      18 Reverse pyramid

---
+++ src/demopack/assignment1pyramid.java
@@ -0,0 +1,29 @@
+package demopack;
+
+public class assignment1pyramid {
+
+    public static void main(String[] args) {
+//        nested loops
+        int k =1;
+        for(int i=0; i<4; i++)  //outer for loop- this block will loop for 4 times
+        {
+            for(int j=1; j<=4-i; j++)  // inner loop
+            {
+                System.out.print(k);
+                System.out.print("\t");
+
+                k++;
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*Result
+1	2	3	4
+5	6	7
+8	9
+10
+
+*/
--- src/demopack/assignment2ReversePyramid.java
+++ src/demopack/assignment2ReversePyramid.java
@@ -1,13 +1,13 @@
 package demopack;
 
-public class nestedforloopassignment {
+public class assignment2ReversePyramid {
 
     public static void main(String[] args) {
 //        nested loops
         int k =1;
-        for(int i=0; i<4; i++)  //outer for loop- this block will loop for 4 times
+        for(int i=1; i<5; i++)  //outer for loop- this block will loop for 4 times
         {
-            for(int j=1; j<=4-i; j++)  // inner loop
+            for(int j=1; j<=i; j++)  // inner loop
             {
                 System.out.print(k);
                 System.out.print("\t");
@@ -21,9 +21,9 @@ public class nestedforloopassignment {
 }
 
 /*Result
-1	2	3	4
-5	6	7
-8	9
-10
+1
+2	3
+4	5	6
+7	8	9	10
 
 */
