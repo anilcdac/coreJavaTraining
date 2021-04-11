4ce4ec75cf174f1213e887a9054ba70676ba350a
anilkalshetti <anilkalshetti@gmail.com>
2021-04-11 19:19:39 +0530

    Notes         //7a] Two ways of defining string object - Interview

---
+++ src/coreJavaTraining/stringclassdemo.java
@@ -0,0 +1,19 @@
+package coreJavaTraining;
+
+public class stringclassdemo {
+
+    public static void main(String[] args) {
+/*String is class in java
+String: Its one of prebuilt class in java    - Interview
+
+How many ways string can be defined
+1] String literal
+2. By creating object of string  */
+
+        String a = "hello" ;  //String literal
+        String ab = new String("hello");   // with String class
+// now this object ab can perform all the string manipulation on this string Hello
+//
+
+    }
+}
