be6d0e89d290ddd020d64b29e26a0d4cfb35687d
anilkalshetti <anilkalshetti@gmail.com>
2021-04-11 22:08:35 +0530

    Notes      12 While Loop-Do While

--- src/coreJavaTraining/whiledemo.java
+++ src/coreJavaTraining/whiledemo.java
@@ -5,13 +5,25 @@ public class whiledemo {
     public static void main(String[] args) {
 // while loop
 
-        int i=0;
-        while(i<=10)
-        {
-            System.out.println(i);
-            i++;
-        }
+//        int i=0;
+//        while(i<=10)
+//        {
+//            System.out.println(i);
+//            i++;
+//        }
 
+
+
+
+
+//Do - while
+
+int j=20;
+do{
+            System.out.println(j);
+            j++;
+    }while(j>30);
+        System.out.println(j);
     }
 }
 /*
