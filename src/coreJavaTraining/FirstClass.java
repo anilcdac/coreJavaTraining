cd08a2eba98630be4d98626adc5bd3067721be16
anilkalshetti <anilkalshetti@gmail.com>
2021-04-11 11:04:52 +0530

    method - return int

--- src/coreJavaTraining/FirstClass.java
+++ src/coreJavaTraining/FirstClass.java
@@ -2,10 +2,15 @@ package coreJavaTraining;
 
 public class FirstClass {
 
-    static int a =4;
+    //Methods
+    public int getData() {
+//        static int a = 4;
+        System.out.println("I am in method");
+        return 2;
+    }
 
     public static void main(String[] args) {
-        System.out.println(a);
+//        System.out.println(a);
         System.out.println("Hello World");
 
     }
