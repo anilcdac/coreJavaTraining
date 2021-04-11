93a855a0b9dff618a0788f26f730b8a777bcb24c
anilkalshetti <anilkalshetti@gmail.com>
2021-04-11 22:01:15 +0530

    Notes      12 While Loop

---
+++ src/coreJavaTraining/whiledemo.java
@@ -0,0 +1,21 @@
+package coreJavaTraining;
+
+public class whiledemo {
+
+    public static void main(String[] args) {
+// while loop
+
+        int i=0;
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
+/*
+Result
+
+
+ */
