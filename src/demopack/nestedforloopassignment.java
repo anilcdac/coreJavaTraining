236764e307823325a6d2712538b181f66861ad9e
anilkalshetti <anilkalshetti@gmail.com>
2021-04-11 22:49:02 +0530

    Notes      17 pyramid

--- src/demopack/nestedforloopassignment.java
+++ src/demopack/nestedforloopassignment.java
@@ -7,13 +7,13 @@ public class nestedforloopassignment {
         int k =1;
         for(int i=0; i<4; i++)  //outer for loop- this block will loop for 4 times
         {
-            System.out.println();
             for(int j=1; j<=4-i; j++)  // inner loop
             {
                 System.out.print(k);
                 k++;
 
             }
+            System.out.println();
         }
     }
 }
