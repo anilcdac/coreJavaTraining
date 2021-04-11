ab5735d31521e2a9bd0ae0cdb59cdd8c4323562d
anilkalshetti <anilkalshetti@gmail.com>
2021-04-11 21:49:31 +0530

    Notes      11 For Loop

---
+++ src/coreJavaTraining/collectiondemo.java
@@ -0,0 +1,20 @@
+package coreJavaTraining;
+
+import java.util.Locale;
+
+public class collectiondemo {
+
+    public static void main(String[] args) {
+
+        int a[] = {4,5,5,5,4,6,6,9,4};
+//        print unique number from the list - amazon
+//        print the string in reverse  - yahoo
+
+
+    }
+}
+/*
+Result
+
+
+ */
---
+++ src/coreJavaTraining/forloopexample.java
@@ -0,0 +1,35 @@
+package coreJavaTraining;
+
+public class forloopexample {
+
+    public static void main(String[] args) {
+
+        //1 to 10
+//        for (Initialization;condition;increment)
+        for ( int i=0; i<10; i++)
+        {
+            if(i==9)
+          System.out.println(i);
+            else
+                System.out.println("I didn't find number");
+
+        }
+
+//        If -else
+        if(5>2) {
+            System.out.println("Success");
+        }
+        else{
+                System.out.println("fail");
+
+            }
+
+
+
+    }
+}
+/*
+Result
+
+
+ */
