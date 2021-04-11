62809020e257204f68d21016627410ef59681738
anilkalshetti <anilkalshetti@gmail.com>
2021-04-11 15:20:41 +0530

    Syntax for-Creating object for class

--- src/coreJavaTraining/FirstClass.java
+++ src/coreJavaTraining/FirstClass.java
@@ -3,14 +3,17 @@ package coreJavaTraining;
 public class FirstClass {
 
     //Methods
-    public String getData() {
+    public void getData() {
 //        static int a = 4;
         System.out.println("I am in method");
-        return "hello";
+//        return "hello";
     }
 
     public static void main(String[] args) {
-//        System.out.println(a);
+      FirstClass fn = new FirstClass();
+
+
+        //        System.out.println(a);
         System.out.println("Hello World");
 
     }
