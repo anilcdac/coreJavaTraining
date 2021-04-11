596a700bd87cc40f7457aa50e9f8a2157e79902a
anilkalshetti <anilkalshetti@gmail.com>
2021-04-11 10:55:37 +0530

    static int

--- src/coreJavaTraining/FirstClass.java
+++ src/coreJavaTraining/FirstClass.java
@@ -2,7 +2,11 @@ package coreJavaTraining;
 
 public class FirstClass {
 
+    static int a =4;
+
     public static void main(String[] args) {
+        System.out.println(a);
         System.out.println("Hello World");
+
     }
 }
