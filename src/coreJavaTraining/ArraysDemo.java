1c234bb8efea9afa22e9561e5f2061138edf3a85
anilkalshetti <anilkalshetti@gmail.com>
2021-04-12 23:12:25 +0530

    Notes 31 Array- Dynamic allocation

--- src/coreJavaTraining/ArraysDemo.java
+++ src/coreJavaTraining/ArraysDemo.java
@@ -8,16 +8,25 @@ public class ArraysDemo {
 //        a =4 ;
 //        Arrays- A container which stores multiple values of same data type
 
-        int a[] = new int[5]; // decalres an array and allocates memory for the values
+//        way1
+        int a[] = new int[5]; // declares an array and allocates memory for the values
         a[0] = 2;
         a[1] = 6;
         a[2] = 1;
         a[3] = 9;
         a[4] = 12;  // initialised values into that array
 
+//        way2
+        int b[] = {1,4,3,5,7,8}; //b. memory is allocated dynamically
+
         for(int i = 0; i<a.length; i++)
         {
             System.out.println(a[i]); // retrieve values present in the array
+        }
+
+        for(int i = 0; i<b.length; i++)
+        {
+            System.out.println(b[i]); // retrieve values present in the array
         }
 
     }
