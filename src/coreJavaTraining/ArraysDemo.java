69df031476a9eed173f23ad640491f0a721b99eb
anilkalshetti <anilkalshetti@gmail.com>
2021-04-12 23:22:03 +0530

    Notes 31 Array- Mutidimentional

--- src/coreJavaTraining/ArraysDemo.java
+++ src/coreJavaTraining/ArraysDemo.java
@@ -3,31 +3,26 @@ package coreJavaTraining;
 public class ArraysDemo {
 
     public static void main(String[] args) {
+//Multidimentional array - row, column
 
-//        int a = 4;
-//        a =4 ;
-//        Arrays- A container which stores multiple values of same data type
+        int a[][] = new int[2][3];
+        a[0][0]=2;
+        a[0][1]=4;
+        a[0][2]=5;
+        a[1][0]=3;
+        a[1][1]=4;
+        a[1][2]=7;
+        int b[][] = {{2,4,5},{3,4,7}}; // way2 of writing
 
-//        way1
-        int a[] = new int[5]; // declares an array and allocates memory for the values
-        a[0] = 2;
-        a[1] = 6;
-        a[2] = 1;
-        a[3] = 9;
-        a[4] = 12;  // initialised values into that array
-
-//        way2
-        int b[] = {1,4,3,5,7,8}; //b. memory is allocated dynamically
-
-        for(int i = 0; i<a.length; i++)
-        {
-            System.out.println(a[i]); // retrieve values present in the array
-        }
-
-        for(int i = 0; i<b.length; i++)
-        {
-            System.out.println(b[i]); // retrieve values present in the array
-        }
+        System.out.println(a[1][0]);
+        System.out.println("");
+        System.out.println(b[1][0]);
 
     }
 }
+
+/*
+ result
+3
+*/
+
