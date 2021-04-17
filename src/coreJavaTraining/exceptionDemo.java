1dc7df81e20651f79db5f5c3e313756b3cf65110
anilkalshetti <anilkalshetti@gmail.com>
2021-04-17 22:37:58 +0530

    Notes   section 14 Finally

--- src/coreJavaTraining/exceptionDemo.java
+++ src/coreJavaTraining/exceptionDemo.java
@@ -9,13 +9,30 @@ public class exceptionDemo {
         int c = 0;
         try
         {
-            int k = b/c;
+//            int k = b/c;
+            int arr[] = new int[5];
+            System.out.println(arr[7]);
 
         }
-        catch(Exception e)
+        catch(ArithmeticException et)
+        {
+            System.out.println("I catch Arithmetic exception");
+        }
+        catch(IndexOutOfBoundsException ets)
+        {
+            System.out.println("I catched Index out of bound exception");
+        }
+
+        catch(Exception e)  // accepts all kinds of exception- general
         {
             System.out.println("I catched error/exception");
         }
-
+        finally
+        {
+            //though exception found,script failed,  it will come to finally , and execute that finally block
+            System.out.println("I am in finally- delete cookies- close browser");
+        }
     }
 }
+
+//if you stop jvm forcefully, clicking stop, finally will not execute3
