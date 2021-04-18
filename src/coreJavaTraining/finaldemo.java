ab983301fc57a05f7eeb6893541a772762dd3c6f
anilkalshetti <anilkalshetti@gmail.com>
2021-04-18 22:05:09 +0530

    Notes   section 65 Final Keyword

---
+++ src/coreJavaTraining/finaldemo.java
@@ -0,0 +1,15 @@
+package coreJavaTraining;
+
+public class finaldemo {
+
+    /* final is about restricting access
+    also methods / class can be marked as final
+    if you mark class name as final, u cannot extend it.
+    if you mark method as final, u cannot override that
+*/
+
+    public static void main(String[] args) {
+       final int i =4; // constant variables
+      // i=5;  //Cannot assign a value to final variable 'i'
+    }
+}
